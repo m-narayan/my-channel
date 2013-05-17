8ad48905804ddf008232546abab1223aaeb37d0e
RamakrishnanK <ramakrishnan@arrivusystems.com>
2013-05-17 15:00:50 +0530

    session changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130515120427) do
+ActiveRecord::Schema.define(:version => 20130517092936) do
 
   create_table "activities", :force => true do |t|
     t.integer  "activity_verb_id"
@@ -343,6 +343,16 @@ ActiveRecord::Schema.define(:version => 20130515120427) do
   end
 
   add_index "rooms", ["actor_id"], :name => "index_rooms_on_actor_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "sites", :force => true do |t|
     t.text     "config"
