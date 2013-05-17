172e7b764653eba537c93e240a4988735d249513
RamakrishnanK <ramakrishnan@arrivusystems.com>
2013-05-17 15:54:49 +0530

    session changes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Channelyst::Application.config.session_store :cookie_store, key: '_retail_net_session'
+Channelyst::Application.config.session_store :cookie_store, key: '_channelyst_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
