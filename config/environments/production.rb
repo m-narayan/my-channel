40b45f373ec6ea339b32b9a2eefef3c480992b15
RamakrishnanK <ramakrishnan@arrivusystems.com>
2013-05-17 17:29:08 +0530

    asset precompile enable production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Channelyst::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -50,7 +50,7 @@ Channelyst::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-
+config.action_mailer.default_url_options = { :host => 'channelyst-demo.herokuapp.com/'}
   # Enable threaded mode
   # config.threadsafe!
 
